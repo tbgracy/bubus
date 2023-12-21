7e580e54d2e5bdef0f52f245430d5534556a587a
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2023-12-21 21:20:05 +0300

    ui: Change link color and body height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ export default function Footer() {
     const currentYear = (new Date()).getFullYear();
 
     return <footer>
-        &copy; {currentYear} - tbgracy
+        &copy; {currentYear} - Made with 👐 by <a href="https://github.com/tbgracy">@tbgracy</a>
     </footer>
 }
