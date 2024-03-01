76d9a5c8c1ef4f41d694ba8009a973ffbc7862fb
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-03-01 09:38:58 +0300

    fix: Move loading gif to public folder to try to improve it's loading time

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,4 +1,3 @@
-import loadingAnimation from './assets/loading.gif'
 import BusItem from './components/BusItem'
 import { GoUpBottom } from './components/GoUpButton'
 import Bus from './types/Bus'
@@ -12,9 +11,11 @@ const noBusMessage = "Il n'y a pas de bus  pour ce trajet, veuillez choisir d'au
 const initialMessage = "Le résultat de recherche sera affiché ici.";
 
 export default function Result({ isLoading, buses }: Props) {
+    const loadingAnimationPath = '/loading.gif'
+
     return <section id="result" className="result">
         {isLoading
-            ? <img src={loadingAnimation} />
+            ? <img src={loadingAnimationPath} />
             : buses === undefined ? <p>{initialMessage}</p> : <>
                 <ul className='buses'>
                     {buses!.length > 0 ? buses!.map(b => <BusItem bus={b} />) : <p>{noBusMessage}</p>}
